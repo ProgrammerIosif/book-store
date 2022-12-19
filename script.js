a7c5dc2f24124cbcbaceaf94a48f6f8658b1181d
ProgrammerIosif <programmeriosif@protonmail.com>
2022-12-19 21:51:26 +0200

    added stylings

--- script.js
+++ script.js
@@ -34,25 +34,39 @@ function displayBook(book) {
     pages.textContent = `${book.pages} pages`;
     card.appendChild(pages);
 
+    const status = document.createElement('button');
+    status.classList.add('status');
+    status.textContent = book.read === true ? 'STATUS: READ' : 'STATUS: NOT READ';
+    card.appendChild(status);
+
     const remove = document.createElement('button');
     remove.classList.add('removeBook');
     remove.id = book.title;
-    remove.textContent = 'REMOVE';
+    remove.textContent = 'REMOVE BOOK';
     card.appendChild(remove);
     remove.addEventListener('click', () => {
             myLibrary = myLibrary.filter(b => b.title != remove.id);
             displayBooks();
         })
 
-    const status = document.createElement('button');
-    status.classList.add('status');
-    status.textContent = book.read === true ? 'read' : 'not read';
-    card.appendChild(status);
-
     booksList.appendChild(card);
 }
 
-let myLibrary = [];
+let myLibrary = [
+    new Book('Afds of Sofsd','Foijew Jioqwe','390',true),
+    new Book('Afaier Afjaiowe', 'Afdsaf De Fewqioj', '800', false),
+    new Book('SDFHIOS HFISOH','Apgre Sfsdi', '193', true),
+    new Book('Afds of Sofsd','Foijew Jioqwe','390',true),
+    new Book('Afaier Afjaiowe', 'Afdsaf De Fewqioj', '800', false),
+    new Book('SDFHIOS HFISOH','Apgre Sfsdi', '193', true),
+    new Book('Afds of Sofsd','Foijew Jioqwe','390',true),
+    new Book('Afaier Afjaiowe', 'Afdsaf De Fewqioj', '800', false),
+    new Book('SDFHIOS HFISOH','Apgre Sfsdi', '193', true),
+    new Book('Afds of Sofsd','Foijew Jioqwe','390',true),
+    new Book('Afaier Afjaiowe', 'Afdsaf De Fewqioj', '800', false),
+    new Book('SDFHIOS HFISOH','Apgre Sfsdi', '193', true)
+];
+
 const booksList = document.getElementById('container');
 
 const form = document.querySelector('form');
@@ -61,6 +75,7 @@ let currentPrompt = form;
 displayBooks();
 
 const addBookButton = document.createElement('button');
+addBookButton.classList.add('addBook');
 addBookButton.textContent = 'ADD BOOK';
 currentPrompt = addBookButton;
 displayBooks();
